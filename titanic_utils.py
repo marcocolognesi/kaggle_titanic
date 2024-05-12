49a4a7ce69b5e8cdfaac9793a2958de4524c1655
Marco Colognesi <marcocolognesi5@gmail.com>
2024-05-12 17:17:38 +0200

    aggiunto funzione per trovare età media di ogni titolo nominativo

--- titanic_utils.py
+++ titanic_utils.py
@@ -41,7 +41,7 @@ def get_eda_insights(titanic_train_dataset: pd.DataFrame, variable: str) -> pd.D
     variable_insights_df = variable_sum_groupby.copy(deep=True)
 
     # find amount of passengers for each label in the variable
-    variable_insights_df['Passengers'] = variable_count_groupby['PassengerId']
+    variable_insights_df['Passengers'] = variable_count_groupby['Survived']
 
     # find percentage of passengers in each label in the variable (with respect to their total amount)
     variable_insights_df['%_Passengers'] = [x / variable_insights_df['Passengers'].sum() for x in variable_insights_df['Passengers']]
@@ -94,15 +94,34 @@ def add_name_title_column(titanic_df: pd.DataFrame, column_with_names: str) -> p
     Returns:
         pd.DataFrame: _description_
     """
-    # first split by ','
+    # name example: Braund, Mr. Owen Harris -> we need to extract 'Mr' from the name
+
+    # first split by ',' -> ['Braund', ' Mr. Owen Harris']
     first_split = [x.split(',')[1] for x in titanic_df[column_with_names]]
 
-    # second split by '.'
+    # second split by '.' -> [' Mr', ' Owen Harris']
     second_split = [x.split('.')[0] for x in first_split]
 
-    # delete empty space at the beginning using replace
+    # delete empty space at the beginning using replace -> from ' Mr' to 'Mr'
     title_names = [x.replace(' ', '') for x in second_split]
 
     # add new column containing titles
     titanic_df['Name_title'] = title_names
     return titanic_df
+
+
+def find_avg_age_title_names(titanic_df: pd.DataFrame) -> pd.Series:
+    """_summary_
+
+    Args:
+        titanic_df (pd.DataFrame): _description_
+
+    Returns:
+        pd.Series: _description_
+    """
+    # mean groupby 'Name_title'
+    _, name_title_mean_groupby, _ = sum_mean_and_count_groupby(train_df=titanic_df, variable='Name_title')
+
+    # extrapolate 'Age' column in the groupy
+    name_title_avg_age = name_title_mean_groupby['Age']
+    return name_title_avg_age
